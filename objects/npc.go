359ddcf9f04853867908ecfbaaf49fc08ef90a1e
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-05-30 21:52:14 +0100

    Seperated player object in to multiple objects

--- objects/npc.go
+++ objects/npc.go
@@ -1,1 +1,67 @@
 package objects
+
+import (
+	"image/color"
+	"github.com/hajimehoshi/ebiten"
+)
+
+type Npc struct {
+	Object
+	Velocity                                Vector2D
+	Speed, MaxVelocity, Gravity, Resistance float64
+}
+
+func (n *Npc) init() {
+	n.Speed = 0.3
+	n.Gravity = 0.1
+	n.Resistance = 0.1
+	n.MaxVelocity = 5
+	n.Colour = color.NRGBA{0x00, 0xfa, 0x9f, 0xff}
+	n.Opts = &ebiten.DrawImageOptions{}
+	n.Opts.GeoM.Translate(10, 10)
+	w, h := n.Sprite.Size()
+	n.Size = Vector2D{float64(w), float64(h)}
+	n.CollisionBox = newAABB(Vector2D{0,0}, n.Size.div(Vector2D{2, 2}))
+}
+
+func (n *Npc) Display(layer *ebiten.Image) {
+	n.Opts.GeoM.Translate(n.Velocity.x, n.Velocity.y)
+	n.Object.Display(layer)
+
+}
+
+func (n *Npc) GetVelocity() Vector2D {
+	return n.Velocity
+}
+
+
+func (n *Npc) ApplyResistance() {
+	if n.Velocity.x < 0 {
+		if n.Velocity.x > -n.Speed {
+			n.Velocity.x = 0
+		} else {
+			n.Velocity.x += n.Resistance
+		}
+	} else {
+		if n.Velocity.x < n.Speed {
+			n.Velocity.x = 0
+		} else {
+			n.Velocity.x -= n.Resistance
+		}
+	}
+	if n.Velocity.y < 0 {
+		if n.Velocity.y > -n.Speed {
+			n.Velocity.y = 0
+		} else {
+			n.Velocity.y += n.Gravity
+		}
+	} else {
+		if n.Velocity.y < n.Speed {
+			n.Velocity.y = 0
+		} else {
+			n.Velocity.y -= n.Gravity
+		}
+	}
+}
+
+
---
+++ objects/object.go
@@ -0,0 +1,54 @@
+package objects
+
+import (
+	"github.com/hajimehoshi/ebiten"
+	"image/color"
+)
+
+type Object struct {
+	CollisionBox							*AABB
+	Sprite                                  *ebiten.Image
+	Colour                                  color.NRGBA
+	Opts                                    *ebiten.DrawImageOptions
+	Size 									Vector2D
+}
+
+
+func (o *Object) GetPosition() Vector2D {
+	x := o.Opts.GeoM.Element(0, 2)
+	y := o.Opts.GeoM.Element(1, 2)
+	return Vector2D{x, y}
+}
+
+func (o *Object) SetPosition(d Vector2D) {
+	o.Opts.GeoM.Translate(d.x, d.y)
+}
+
+func (o *Object) AppendPosition(d Vector2D) {
+	newPos := o.GetPosition()
+	newPos.add(d)
+	o.Opts.GeoM.Translate(newPos.x, newPos.y)
+}
+
+func (o *Object) GetSprite() *ebiten.Image {
+	return o.Sprite
+}
+
+func (o *Object) Display(layer *ebiten.Image) {
+	o.CollisionBox.center = o.GetPosition().add(o.Size.div(Vector2D{2, 2}))
+	o.Sprite.Fill(o.Colour)
+	layer.DrawImage(o.Sprite, o.Opts)
+}
+
+func NewObject(colour color.NRGBA, posx float64, posy float64, sprite *ebiten.Image) *Object{
+	o := &Object{}
+	o.Sprite = sprite
+	o.Colour = colour
+	o.Opts = &ebiten.DrawImageOptions{}
+	pos := Vector2D{posx, posy}
+	o.Opts.GeoM.Translate(posx, posy)
+	w, h := o.Sprite.Size()
+	o.Size = Vector2D{float64(w), float64(h)}
+	o.CollisionBox = newAABB(pos.add(o.Size.div(Vector2D{2, 2})), o.Size.div(Vector2D{2, 2}))
+	return o
+}
