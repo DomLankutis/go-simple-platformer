5d50886cbcc11df6a0305950c69e1635ed32c82c
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-05-30 21:53:00 +0100

    Added vector arithmetics

--- objects/structs.go
+++ objects/structs.go
@@ -4,3 +4,21 @@ type Vector2D struct {
 	x float64
 	y float64
 }
+
+// Returns a new vector which is the result of the two vectors
+func (v Vector2D) add(d Vector2D) Vector2D {
+	return Vector2D{v.x + d.x, v.y + d.y}
+}
+
+// Returns the result of dividing the vector by another vector
+func (v Vector2D) div(d Vector2D) Vector2D {
+	return Vector2D{v.x / d.x, v.y / d.y}
+}
+
+func (v Vector2D) sub(d Vector2D) Vector2D {
+	return Vector2D{v.x - d.x, v.y - d.y}
+}
+
+func (v Vector2D) mul(d Vector2D) Vector2D {
+	return Vector2D{v.x * d.x , v.y * d.y}
+}
