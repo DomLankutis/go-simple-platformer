f12affb744394393d41d3ee052f9b2c731ab810a
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-06-05 20:29:51 +0100

    Added an additional collision object

--- main.go
+++ main.go
@@ -9,23 +9,23 @@ import (
 )
 
 var player objects.Player
-var col objects.Object
+var col, col1 objects.Object
 
 func update(screen *ebiten.Image) error {
 	screen.Fill(color.NRGBA{0xa0, 0x01, 0xfa, 0xff})
 
 	player.Move(screen)
-	player.Display(screen)
-	col.Display(screen)
-
-	player.Colour = color.NRGBA{0x0f, 0xff, 0xaf, 0xff}
 	player.Collide(col)
-	player.CollisionBox.Draw(screen)
+	player.Collide(col1)
+
+	col.Display(screen)
+	col1.Display(screen)
+	player.Display(screen)
+
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("Position: %0.f", player.GetPosition()))
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("\nVelocity: %.0f", player.Velocity))
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("\n\nFPS: %.1f", ebiten.CurrentFPS()))
-
 
 	return nil
 }
@@ -33,6 +33,7 @@ func update(screen *ebiten.Image) error {
 func main() {
 	objImage, _ := ebiten.NewImage(90, 80, ebiten.FilterNearest)
 	col = *objects.NewObject(color.NRGBA{0xf0, 0xff,0xaf, 0xff}, 640 / 2, 360 / 2, objImage)
+	col1 = *objects.NewObject(color.NRGBA{0xf0, 0xff, 0xaf, 0xff}, 640 / 2 +90, 360 / 2 + 50, objImage)
 	player = *objects.NewPlayer(ebiten.NewImage(50, 50, ebiten.FilterNearest))
 	if err := ebiten.Run(update, 640, 360, 3, "Game"); err != nil {
 		fmt.Println(err)
