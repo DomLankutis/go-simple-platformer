8b1cbba51db3cc60bde42aa4a8840be343d1c43a
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-06-05 20:34:11 +0100

    Can now get dotProduct, tangent, length. Aswell as being able to normalise the vector and multiply to a single float and check if the vector 'isZero'

--- objects/structs.go
+++ objects/structs.go
@@ -1,16 +1,32 @@
 package objects
 
+import "math"
+
 type Vector2D struct {
 	x float64
 	y float64
 }
 
-// Returns a new vector which is the result of the two vectors
+func (v Vector2D) getLength() float64 {
+	return math.Sqrt((v.x * v.x) + (v.y * v.y))
+}
+
+func (v Vector2D) getNormalised() Vector2D {
+	length := v.getLength()
+	if length != 0 {
+		return Vector2D{v.x / length, v.y / length}
+	}
+	return Vector2D{0,0}
+}
+
+func (v Vector2D) getTangent() Vector2D {
+	return Vector2D{-v.y, v.x}
+}
+
 func (v Vector2D) add(d Vector2D) Vector2D {
 	return Vector2D{v.x + d.x, v.y + d.y}
 }
 
-// Returns the result of dividing the vector by another vector
 func (v Vector2D) div(d Vector2D) Vector2D {
 	return Vector2D{v.x / d.x, v.y / d.y}
 }
@@ -21,4 +37,16 @@ func (v Vector2D) sub(d Vector2D) Vector2D {
 
 func (v Vector2D) mul(d Vector2D) Vector2D {
 	return Vector2D{v.x * d.x , v.y * d.y}
+}
+
+func (v Vector2D) isZero() bool {
+	return v.x == 0 && v.y == 0
+}
+
+func (v Vector2D) mulToFloat(d Vector2D) float64 {
+	return v.x * d.x - v.y * d.y
+}
+
+func (v Vector2D) dotProduct(d Vector2D) float64 {
+	return v.x * d.x + v.y * d.y
 }
