a2974456b9447e96fccb9e1b37b102f0368a9a86
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-06-10 00:23:44 +0100

    Sweept AABB needs to be fixed and Collision() function now returns a bool if a collision has occured

--- objects/AABB.go
+++ objects/AABB.go
@@ -38,10 +38,10 @@ func newAABB(center, extents Vector2D, physics ...Vector2D) *AABB {
 }
 
 func (a *AABB) Draw(layer *ebiten.Image) {
-	img, _ := ebiten.NewImage(int(math.Abs(a.GetSize().x)), int(math.Abs(a.GetSize().y)), ebiten.FilterNearest)
+	img, _ := ebiten.NewImage(int(math.Abs(a.GetSize().X)), int(math.Abs(a.GetSize().Y)), ebiten.FilterNearest)
 	temp := ebiten.DrawImageOptions{}
 	img.Fill(color.NRGBA{0xaf, 0x00, 0x0b, 0x5f})
-	temp.GeoM.Translate(a.GetMin().x, a.GetMin().y)
+	temp.GeoM.Translate(a.GetMin().X, a.GetMin().Y)
 	layer.DrawImage(img, &temp)
 }
 
@@ -55,22 +55,22 @@ func (a *AABB) minkowskiDifference(obj *AABB) *AABB {
 func (a *AABB) closestPointOnBoundsToPoint(point Vector2D) Vector2D{
 	max := a.GetMax()
 	min := a.GetMin()
-	minDist := math.Abs(point.x - min.x)
-	boundsPoint := Vector2D{min.x, point.y}
+	minDist := math.Abs(point.X - min.X)
+	boundsPoint := Vector2D{min.X, point.Y}
 
-	if math.Abs(max.x - point.x) < minDist {
-		minDist = math.Abs(max.x - point.x)
-		boundsPoint = Vector2D{max.x, point.y}
+	if math.Abs(max.X- point.X) < minDist {
+		minDist = math.Abs(max.X - point.X)
+		boundsPoint = Vector2D{max.X, point.Y}
 	}
 
-	if math.Abs(max.y - point.y) < minDist {
-		minDist = math.Abs(max.y - point.y)
-		boundsPoint = Vector2D{point.x, max.y}
+	if math.Abs(max.Y- point.Y) < minDist {
+		minDist = math.Abs(max.Y - point.Y)
+		boundsPoint = Vector2D{point.X, max.Y}
 	}
 
-	if math.Abs(min.y - point.y) < minDist {
-		minDist = math.Abs(min.y - point.y)
-		boundsPoint = Vector2D{point.x, min.y}
+	if math.Abs(min.Y- point.Y) < minDist {
+		minDist = math.Abs(min.Y - point.Y)
+		boundsPoint = Vector2D{point.X, min.Y}
 	}
 
 	return boundsPoint
@@ -105,15 +105,15 @@ func (a *AABB) getRayIntersectionFraction(origin, direction Vector2D) float64 {
 	min := a.GetMin()
 	max := a.GetMax()
 	minT := a.getRayIntersectionFractionOfFirstRay(origin, end, min, max)
-	x := a.getRayIntersectionFractionOfFirstRay(origin, end, Vector2D{min.x, max.y}, Vector2D{max.x, max.y})
+	x := a.getRayIntersectionFractionOfFirstRay(origin, end, Vector2D{min.X, max.Y}, Vector2D{max.X, max.Y})
 	if x < minT {
 		minT = x
 	}
-	x = a.getRayIntersectionFractionOfFirstRay(origin, end, Vector2D{max.x, max.y}, Vector2D{max.x, min.y})
+	x = a.getRayIntersectionFractionOfFirstRay(origin, end, Vector2D{max.X, max.Y}, Vector2D{max.X, min.Y})
 	if x < minT {
 		minT = x
 	}
-	x = a.getRayIntersectionFractionOfFirstRay(origin, end, Vector2D{max.x, max.y}, Vector2D{max.x, min.y})
+	x = a.getRayIntersectionFractionOfFirstRay(origin, end, Vector2D{max.X, max.Y}, Vector2D{max.X, min.Y})
 	if (x < minT) {
 		minT = x
 	}
@@ -123,10 +123,10 @@ func (a *AABB) getRayIntersectionFraction(origin, direction Vector2D) float64 {
 
 func (o *Object) IsOverlapping(obj Object) (bool, *AABB){
 	diff := obj.CollisionBox.minkowskiDifference(o.CollisionBox)
-	return diff.GetMin().x <= 0 && diff.GetMax().x >= 0 && diff.GetMin().y <= 0 && diff.GetMax().y >= 0, diff
+	return diff.GetMin().X <= 0 && diff.GetMax().X >= 0 && diff.GetMin().Y <= 0 && diff.GetMax().Y >= 0, diff
 }
 
-func (o *Object) Collide(obj Object) {
+func (o *Object) Collide(obj Object) bool {
 
 	overlapping, diff := o.IsOverlapping(obj)
 
@@ -134,7 +134,7 @@ func (o *Object) Collide(obj Object) {
 
 		penetrationVector := diff.closestPointOnBoundsToPoint(Vector2D{0, 0})
 
-		o.SetPosition(penetrationVector)
+		o.ApplyDirectVelocity(penetrationVector)
 
 		if !penetrationVector.isZero() {
 			tangent := (penetrationVector.getNormalised()).getTangent()
@@ -144,16 +144,22 @@ func (o *Object) Collide(obj Object) {
 
 			o.Velocity = Vector2D{oDotProduct, oDotProduct}.mul(tangent)
 			obj.Velocity = Vector2D{objDotProduct, objDotProduct}.mul(tangent)
-			o.CanJump = true
+
+			if penetrationVector.Y < 0 || penetrationVector.X != 0 {
+				o.CanJump = true
+			}
+			return true
 		}
 	}else {
 		relativeMotion := o.Velocity.sub(obj.Velocity)
-		h := diff.getRayIntersectionFraction(Vector2D{0, 0}, relativeMotion)
+		//h := diff.getRayIntersectionFraction(Vector2D{0, 0}, relativeMotion)
+
+		h := math.Inf(1)
 
 		if h < math.Inf(1) {
 
-			o.SetPosition(o.Velocity.mul(Vector2D{h,h}))
-			obj.SetPosition(obj.Velocity.mul(Vector2D{h,h}))
+			o.ApplyDirectVelocity(o.Velocity.mul(Vector2D{h,h}))
+			obj.ApplyDirectVelocity(obj.Velocity.mul(Vector2D{h,h}))
 
 
 			tangent := (relativeMotion.getNormalised()).getTangent()
@@ -161,10 +167,12 @@ func (o *Object) Collide(obj Object) {
 			oDotProduct := o.Velocity.dotProduct(tangent)
 			objDotProduct := obj.Velocity.dotProduct(tangent)
 
-			o.SetPosition(Vector2D{oDotProduct, oDotProduct}.mul(tangent))
-			obj.SetPosition(Vector2D{objDotProduct, objDotProduct}.mul(tangent))
+			o.ApplyDirectVelocity(Vector2D{oDotProduct, oDotProduct}.mul(tangent))
+			obj.ApplyDirectVelocity(Vector2D{objDotProduct, objDotProduct}.mul(tangent))
 			o.CanJump = true
+
+			return true
 		}
 	}
-
+	return false
 }
