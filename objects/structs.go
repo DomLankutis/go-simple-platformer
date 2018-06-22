5fcd14f2b1fdc69b503b3d10b5b38ac079091085
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-06-22 13:36:26 +0100

    Made most vector arithmetics global

--- objects/structs.go
+++ objects/structs.go
@@ -7,39 +7,39 @@ type Vector2D struct {
 	Y float64
 }
 
-func (v Vector2D) getLength() float64 {
+func (v Vector2D) GetLength() float64 {
 	return math.Sqrt((v.X * v.X) + (v.Y * v.Y))
 }
 
-func (v Vector2D) getNormalised() Vector2D {
-	length := v.getLength()
+func (v Vector2D) GetNormalised() Vector2D {
+	length := v.GetLength()
 	if length != 0 {
 		return Vector2D{v.X / length, v.Y / length}
 	}
 	return Vector2D{0,0}
 }
 
-func (v Vector2D) getTangent() Vector2D {
+func (v Vector2D) GetTangent() Vector2D {
 	return Vector2D{-v.Y, v.X}
 }
 
-func (v Vector2D) add(d Vector2D) Vector2D {
+func (v Vector2D) Add(d Vector2D) Vector2D {
 	return Vector2D{v.X + d.X, v.Y + d.Y}
 }
 
-func (v Vector2D) div(d Vector2D) Vector2D {
+func (v Vector2D) Div(d Vector2D) Vector2D {
 	return Vector2D{v.X / d.X, v.Y / d.Y}
 }
 
-func (v Vector2D) sub(d Vector2D) Vector2D {
+func (v Vector2D) Sub(d Vector2D) Vector2D {
 	return Vector2D{v.X - d.X, v.Y - d.Y}
 }
 
-func (v Vector2D) mul(d Vector2D) Vector2D {
+func (v Vector2D) Mul(d Vector2D) Vector2D {
 	return Vector2D{v.X * d.X, v.Y * d.Y}
 }
 
-func (v Vector2D) isZero() bool {
+func (v Vector2D) IsZero() bool {
 	return v.X == 0 && v.Y == 0
 }
 
